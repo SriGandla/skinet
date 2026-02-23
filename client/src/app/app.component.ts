3364e9a6454c3cad91e0281f98fef45a6b998624
Srikanth Gandla <contactsrikanth123@gmail.com>
2026-02-23 11:01:35 -0600

    End of Section 9

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -4,24 +4,15 @@ import { HeaderComponent } from "./layout/header/header.component";
 import { HttpClient } from '@angular/common/http';
 import { Product } from './shared/models/product';
 import { Pagination } from './shared/models/pagination';
+import { ShopService } from './core/services/shop.service';
+import { ShopComponent } from "./features/shop/shop.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, ShopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
-  baseurl = 'https://localhost:5001/api/'
-  private http = inject(HttpClient);
+export class AppComponent  {
   title = 'skinet';
-  products: Product[] = [];
-
-  ngOnInit(): void {
-    this.http.get<Product[]>(this.baseurl + 'Products').subscribe({
-      next: response => this.products = response,
-      error: error => console.log(error),
-      complete: () => console.log('Request completed')
-    })
-  }
 }
---
+++ client/src/app/core/services/shop.service.ts
@@ -0,0 +1,57 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Pagination } from '../../shared/models/pagination';
+import { Product } from '../../shared/models/product';
+import { ShopParams } from '../../shared/models/shopParams';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ShopService {
+  baseurl = 'https://localhost:5001/api/'
+  private http = inject(HttpClient);
+  types: string[] = [];
+  brands: string[] = [];
+
+  getProducts(shopParams: ShopParams) {
+    let params = new HttpParams();
+    console.log('ShopParams in getProducts:', shopParams);
+
+    if (shopParams.brands.length > 0) {
+      params = params.append('brands', shopParams.brands.join(','));
+    }
+
+    if (shopParams.types.length > 0) {
+      params = params.append('types', shopParams.types.join(','));
+    }
+
+    if (shopParams.sort) {
+      params = params.append('sort', shopParams.sort);
+    }
+
+    if (shopParams.search) {
+      params = params.append('search', shopParams.search);
+    }
+
+    params = params.append('pageIndex', shopParams.pageNumber);
+    params = params.append('pageSize', shopParams.pageSize);
+
+    return this.http.get<Pagination<Product>>(this.baseurl + 'Products', { params });
+  }
+
+  getBrands() {
+    if (this.brands.length > 0) return ;
+    return this.http.get<string[]>(this.baseurl + 'Products/brands').subscribe({
+      next: response => this.brands = response,
+      error: error => console.log(error)
+    })
+  }
+
+  getTypes() {
+    if (this.types.length > 0) return ;
+    return this.http.get<string[]>(this.baseurl + 'Products/types').subscribe({
+      next: response => this.types = response,
+      error: error => console.log(error)
+    })
+  }
+}
